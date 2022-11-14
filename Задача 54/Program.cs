65fc0a75f632e23cff3e8205e7c796e5543a5ed2
SinkevichV <sinkevichvasiliu@mail.ru>
2022-11-14 17:18:38 +0700

    задача 54

---
+++ Задача 54/Program.cs
@@ -0,0 +1,79 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+int[,] IntitMatrix(int m, int n)
+{
+    Random rnd = new();
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 20);
+        }
+    }
+    return matrix;
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    bool isCorrect = false;
+    int result = 0;
+    while (!isCorrect)
+        if (int.TryParse(Console.ReadLine(), out result))
+            isCorrect = true;
+        else
+            Console.WriteLine("Введено не число. Повторите ввод.");
+
+    return result;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] SortValueRow(int[,] matrix)
+{
+    int[,] resultMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                {
+                    if (matrix[i, j] <= matrix[i, k]) continue;
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+        return matrix;
+    }
+
+
+}
+int m = GetNumber("введите число строк ");
+int n = GetNumber("Введите количество столбцов ");
+int[,] matrix = IntitMatrix(m, n);
+PrintArray(matrix);
+int[,] resultMatrix = SortValueRow(matrix);
+PrintArray(resultMatrix);
